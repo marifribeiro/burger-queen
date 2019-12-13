1030da4d5f96c3e7b8d20f8e53751e40084f1b6e
Mariana Ribeiro <mariana-fribeiro@live.com>
2019-12-13 13:41:38 -0300

    menu is now on firebase and showing on screen

--- src/components/Lunch/index.js
+++ src/components/Lunch/index.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
+import firebase from '../../firebase';
 
 import MenuItem from '../MenuItem/index';
 
+function useItems() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    firebase.firestore().collection('lunch').onSnapshot((snap) => {
+      const newItems = snap.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+
+      setItems(newItems);
+    })
+  }, [])
+
+  return items;
+}
+
 function Lunch() {
+  const items = useItems()
+
   return(
     <>
     <h2 class="lunch-title">Almoço e jantar</h2>
     <div className="lunch">
-      <span className="lunch-subtitle">Hambúrgers</span>
-      <span className="section burgers">
-        <MenuItem name="Hambúrguer simples" price="10" />
-        <MenuItem name="Hambúrger duplo" price="15" />
+      <div className="join">
+        <span className="lunch-subtitle">Hambúrgers</span>
+        <span className="lunch-subtitle">Acompanhamentos</span>
+      </div>
+      <span className="section">
+        {items.map((item) => item.type === "burger" ? <MenuItem key={item.id} name={item.name} price={item.price} /> : false)}
+        {items.map((item) => item.type === "side" ? <MenuItem key={item.id} name={item.name} price={item.price} /> : false)}
       </span>
-      <span className="lunch-subtitle">Acompanhamentos</span>
-      <span className="section sides">
-        <MenuItem name="Batata frita" price="5" />
-        <MenuItem name="Anéis de cebola" price="5" />
-      </span>
-      <span className="lunch-subtitle">Bebidas</span>
+      <span className="lunch-subtitle drinks">Bebidas</span>
       <span className="section drinks">
-        <MenuItem name="Água 500ml" price="5" />
-        <MenuItem name="Água 750ml" price="7" />
-        <MenuItem name="Refrigerante 500ml" price="7" />
-        <MenuItem name="Refrigerante 750ml" price="10" />
+        {items.map((item) => item.type === "drink" ? <MenuItem key={item.id} name={item.name} price={item.price} /> : false)}
       </span>
     </div>
     </>
