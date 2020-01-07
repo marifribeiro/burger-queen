07ad4786762cfec6edb84d932214568ef3649091
Mariana Ribeiro <kaleidoscopesland@hotmail.com>
2020-01-07 15:47:27 -0200

    creating checkboxes for burger options

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -3,18 +3,30 @@ import './styles.css';
 
 function Burger(props) {
   return (
-    <>
       <div className='item-container'>
         <div className='order-item'>
           <span className="order-text">{props.name}</span>
           <span className="order-text order-price">R$ {props.price}</span>
+          <div class="control" id='control'>
+            <label className='radio'>
+              <input type='radio' id='beef' value='beef' defaultChecked />
+                Carne
+            </label>
+            <label className='radio'>
+              <input type='radio' id='chicken' value='chicken' />
+                Frango
+            </label>
+            <label className='radio'>
+              <input type='radio' id='veg' value='veg' />
+                Veggie
+            </label>
+          </div>
         </div>
         <div className="order-button order-text order-counter">{props.amount}</div>
         <button className='order-button order-add order-text' onClick={props.handleAdd}></button>
         <button className='order-button order-minus order-text' onClick={props.handleMinus}></button>
         <button className='order-button order-remove order-text' onClick={props.handleRemove}></button>
       </div>
-    </>
   )
 }
 
